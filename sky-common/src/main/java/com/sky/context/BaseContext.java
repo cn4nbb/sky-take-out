1d6b8639c6f72dcaaa4b3bba3dbd1c8831d06eef
cn4bb <wdsg_cnan@163.com>
2025-04-14 16:18:53 +0800

    新增员工功能

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,16 +2,28 @@ package com.sky.context;
 
 public class BaseContext {
 
+    //创建ThreadLocal静态对象
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    /**
+     * 包装ThreadLocal.set方法 存储当前用户的id
+     * @param id
+     */
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
 
+    /**
+     * 包装ThreadLocal.get方法 获取当前用户id
+     * @return
+     */
     public static Long getCurrentId() {
         return threadLocal.get();
     }
 
+    /**
+     * 包装ThreadLocal.remove方法 移除ThreadLocal中存储的值
+     */
     public static void removeCurrentId() {
         threadLocal.remove();
     }
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,28 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 捕获sql异常
+     * @return
+     */
+    @ExceptionHandler
+    public Result sqlExceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //获取异常信息
+        String message = ex.getMessage();
+
+        //判断异常是否为唯一性冲突
+        if (message.contains("Duplicate entry")){
+            //分割信息
+            String[] split = message.split(" ");
+            //拿到用户名
+            String username = split[2];
+            //拼接返回信息
+            String msg = String.join(username, MessageConstant.ACCOUNT_ALREADY_EXIST);
+            //返回错误信息
+            return Result.error(msg);
+        }else {
+            //否则 返回未知错误
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
